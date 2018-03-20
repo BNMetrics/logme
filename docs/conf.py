b6c4c43cba6ea44121212ea6d893c32191167d65
Luna Chen <petitelumiere90@gmail.com>
2018-03-20 12:38:09 +0000

    made changes in docs/conf.py to make logme import available

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+# Insert logme's path into the system
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
